8c2d6041787fcc975107699a792fbf2c858dbe9a
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-15 21:42:39 -0400

    Building out the humidity fetch and if else statements for display changes with value.

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,9 +1,9 @@
 // variables for html sections
-var cityNameInput = document.querySelector(".cityGroup");
+var cityNameInput = document.querySelector("#search-button");
 var cityNameForm = document.querySelector(".form-control");
 var weathContainer = document.querySelector(".weather-container");
 var currentBox = document.querySelector(".current-box");
-var dayCard= document.getElementsByClassName('day-card');
+var dayCard = document.getElementsByClassName('day-card');
 var fivedayRow = document.querySelector('five-day-box');
 
 // variables for appending the document once we have information
@@ -33,23 +33,65 @@ var weatherAsk = function (city) {
 
     var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=7aa46cc940f1317aa850cdc655c09d9e";
 
-    fetch (weatherApi)
+    fetch(weatherApi)
         .then(function (response) {
             if (!response || response.ok) {
                 throw new Error('There was an error');
             };
             return response.json();
-        
+
         })
-        .then (function (response) {
+        .then(function (response) {
             cityDiv.classlist = 'temp-div';
             weathContainer.appendChild(cityDiv);
-        
+
             cityInfoDiv.classList = 'detail-div';
             weathContainer.appendChild(cityInfoDiv)
 
+            nameEl.innerHTML = "<h2 class='secondary-text'>Current Weather for <span class='font-weight-bold'>" + response.name + "</span></h2><br><img class='icon' src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt=Current weather icon/><br><br><h2 class='font-weight-bold secondary-text'>" + date + "</h2><br>";
+            cityDiv.appendChild(nameEl);
 
-        }
+            tempEl.innerHTML = "<h3 class='secondary-text'>Current Temperature:<span class='font-weight-bold'>" + " " + Math.round(response.main.temp) + "&#176F</span></h3><br>";
+
+            humidEl.innerHTML = "<h4 class='secondary-text'>Humidity:<span class='font-weight-bold'>" + " " + response.main.humidity + "%</span></h4>";
+            cityDiv.appendChild(humidEl);
+
+            windEl = "<h4 class='secondary-text'>Wind Speed:<span class='font-weight-bold'>" + " " + Math.round(response.wind.speed) + " MPH</span></h4>";
+            cityDiv.appendChild(windEl);
+
+
+            // grabbing UV information
+            return fetch("https://api.openweathermap.org/data/2.5/uvi?appid=7aa46cc940f1317aa850cdc655c09d9e&lat=" + response.coord.lat + "&lon=" + response.coord.lon);
+        })
+        .then(function (uvFetch) {
+            return uvFetch.json();
+        })
+        .then(function (uvResponse) {
+            uvBubble.setAttribute("id", "uv-index");
+            uvBubble.classList = "seconday-text uv-class";
+            cityDiv.appendChild(uvBubble)
+
+            var uvValue = uvResponse.value;
+            uvIndex.innerHTML = "UV Index: ";
+
+            uvDisplay.setAttribute("id", "uv-index");
+            uvDisplay.innerHTML= uvValue;
+            uvBubble.appendChild(uvIndex);
+            uvBubble.appendChild(uvDisplay);
+
+            if (uvResponse.value > 7) {
+                document.querySelector("#uv-index").classList = "uv-result rounded bg-danger";
+            } else if (uvResponse.value >= 2 && uvResponse.value <= 7) {
+                document.querySelector("#uv-index").classList = "uv-result rounded bg-warning";
+            } else if (uvResponse.value <= 2) {
+                document.querySelector("#uv-index").classList = "uv-result rounded bg-success";
+            }
+
+            return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + uvResponse.lat + "&lon=" + uvResponse.lon + "&appid=7aa46cc940f1317aa850cdc655c09d9e&units=imperial");
+
+        })
+
+}
 }
 
 
@@ -59,15 +101,15 @@ var weatherAsk = function (city) {
 
 var CitySubmitHandler = function (event) {
     event.preventDefault();
-
-    var cityGroup = cityNameInput.value.trim().toUppercase();
+    // search button submit
+    var cityGroup = cityNameInput.value.trim().toUpperCase();
     console.log(cityGroup);
 
     if (citygroup) {
-    getCityInfo(cityGroup);
-    createBtn(cityGroup);
-    searchHist();
-    
+        weatherAsk(cityGroup);
+        createBtn(cityGroup);
+        searchHist();
+
     } else {
         alert('Please enter a city name to see the current forcast.');
     }
@@ -90,5 +132,3 @@ var buttonClickHandler = function (event) {
 }
 
 cityNameInput.addEventListener('click', CitySubmitHandler)
-
-
